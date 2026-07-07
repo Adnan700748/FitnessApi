d9d965bf7c1a117e1875ee25d2b7af52af82c5a8
Adnan700748 <adenejib07@gmail.com>
2026-07-07 23:24:53 +0300

    refactor: use ApplyConfigurationsFromAssembly in DbContext OnModelCreating

--- Data/FitnessDbContext.cs
+++ Data/FitnessDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using FitnessApi.Entities;
+using FitnessApi.Configurations; // optional, but not needed if assembly scanning
 
 namespace FitnessApi.Data;
 
@@ -8,4 +9,12 @@ public class FitnessDbContext(DbContextOptions<FitnessDbContext> options) : DbCo
     public DbSet<Member> Members => Set<Member>();
     public DbSet<FitnessClass> FitnessClasses => Set<FitnessClass>();
     public DbSet<Booking> Bookings => Set<Booking>();
+    public DbSet<Assessment> Assessments => Set<Assessment>();
+    public DbSet<Badge> Badges => Set<Badge>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Scan the assembly for all IEntityTypeConfiguration classes
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FitnessDbContext).Assembly);
+    }
 }
