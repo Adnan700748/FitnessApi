e1191ead5a413ef6bf2e75d29dd5b8cfaca9d443
Adnan700748 <adenejib07@gmail.com>
2026-07-10 23:44:50 +0300

    db: generate migration AddScheduleToClass

---
+++ Migrations/20260710204437_AddScheduleToClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddScheduleToClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DurationMinutes",
+                table: "FitnessClasses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 60);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduledAt",
+                table: "FitnessClasses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DurationMinutes",
+                table: "FitnessClasses");
+
+            migrationBuilder.DropColumn(
+                name: "ScheduledAt",
+                table: "FitnessClasses");
+        }
+    }
+}
